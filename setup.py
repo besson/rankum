b7c083bf4d6118be24b8067cefc32791d3f3d8ce
Felipe Besson <felipe.besson@olx.com>
2020-11-15 17:32:15 +0100

    fixing author

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='rankum',
+      version='0.1',
+      description='Search Rank experimentation in python',
+      packages=['rankum'],
+      zip_safe=False)
