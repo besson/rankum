3cd36a4ef6d6bdbdefa348069c81480ac44b4381
Felipe Besson <flpbesson@gmail.com>
2020-11-16 16:39:43 +0100

    add Solr support

--- rankum/readers.py
+++ rankum/readers.py
@@ -1,9 +1,10 @@
 from typing import List, Iterator
 from rankum.models import Doc
-from rankum.exceptions import MissingDocIdException, EmptyDocListException
+from rankum.exceptions import MissingDocIdException, EmptyDocListException, \
+    MissingDocReaderInfoException, FetchDocListException
 
 import json
-
+import requests
 
 class DocReader:
     """ Reader document list from different sources"""
@@ -52,3 +53,36 @@ class JsonDocReader(DocReader):
             raise EmptyDocListException(message='Document list is invalid')
 
         return doc_dict
+
+
+class SolrDocReader(DocReader):
+    """ Reader document list from a Solr query """
+    def __init__(self, host: str, query: str):
+        if host is None:
+            raise MissingDocReaderInfoException(message='Host name is missing')
+
+        if query is None:
+            raise MissingDocReaderInfoException(message='Query name is missing')
+
+        self.host = host
+        self.query = query
+
+    def to_doc_list(self) -> Iterator[Doc]:
+        """
+        Convert Solr query results into Iterator[Doc]
+        :return doc iterator (Iterator[Doc]): iterator for converted document list
+        """
+        response = requests.get(f'{self.host}/{self.query}')
+
+        if response.status_code is not 200:
+            raise FetchDocListException(message=f'Failed to fetch document from {response.json()}')
+
+        def fetch_docs():
+            docs = response.json().get('response', []).get('docs', [])
+            for _doc in docs:
+                if 'id' not in _doc:
+                    raise MissingDocIdException(message='Field "id" is mandatory for each document')
+
+                yield Doc(id=str(_doc.pop('id')), **_doc)
+
+        return fetch_docs()
